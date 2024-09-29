f43d73165144eab7dca8d3d8603db554ce3686bc
muhamadsaim <saimrao849@gmail.com>
2024-09-29 11:46:14 +0500

    prevent the screen to from overflow

--- app/layouts/dashboardLayouts.js
+++ app/layouts/dashboardLayouts.js
@@ -1,14 +1,13 @@
 import Header from "../components/header";
 import Sidebar from "../components/sidebar";
 
-
 const DashboardLayout = ({ children }) => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex-1 bg-gray-50 min-h-screen">
         <Header />
-        <main className="p-6">{children}</main>
+        <main className="p-6 overflow-y-auto h-[calc(100vh-64px)]">{children}</main>
       </div>
     </div>
   );
